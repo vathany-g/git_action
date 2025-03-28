0ce1856f2ef1542fda7c3a4a02a7d3500cc780b1
vathany-g <vathany1979@hotmail.com>
2025-03-28 15:51:04 +0800

    Fix: Adjusted file structure and verified backend

--- main.tf
+++ main.tf
@@ -4,18 +4,9 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "thila-terraform-state-bucket"
+    bucket = "thila-s3"
     key    = "terraform/state/terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "thila_s3_git"
-
-
-  tags = {
-    Name        = "Mythila"
-    Environment = "Dev"
-  }
-}
